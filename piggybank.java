33dbdd98288a635ee61ec845371ac849c6611e92
SuhaniSharma072 <suhanisharma072@gmail.com>
2025-10-18 21:55:13 -0500

    2nd commit

--- piggybank.java
+++ piggybank.java
@@ -12,24 +12,49 @@ public class piggybank{
         System.out.println("        ╔═══════════════════════════════════════╗");
         System.out.println("        ║              PIGGY BANK               ║");
         System.out.println("        ╚═══════════════════════════════════════╝");
-
-
-        System.out.println("\t\t\t--OPTIONS--");
-        System.out.println("1) Add money");
-        System.out.println("2) Withdraw money");
-        System.out.println("3) Check balance");
-        System.out.println("4) Exit");
+        boolean running=true;
         double balance=0;
-        System.out.print("\nWhat action would you like to perform? ");
-        int action=kb.nextInt();
 
-        if(action==1){
-            System.out.print("Enter amount to deposit:");
+
+        //find way to keep it going//
+        while (running) {
+            System.out.println("\t\t\t--OPTIONS--");
+            System.out.println("1) Add money");
+            System.out.println("2) Withdraw money");
+            System.out.println("3) Check balance");
+            System.out.println("4) Exit");
             
+            System.out.println("\t\t\tBalance: "+"$"+balance);
+            System.out.print("\nWhat action would you like to perform? ");
+            int action=kb.nextInt();
+
+            if(action==1){
+                System.out.print("Enter amount to deposit:");
+                double deposit=kb.nextDouble();
+                balance+=deposit;
+                System.out.println("\t\t\tBalance: "+"$"+balance);
+            
+            }else if(action==2){
+                System.out.print("Enter amount to withdraw:");
+                double withdrawal=kb.nextDouble();
+                balance-=withdrawal;
+                System.out.println("\t\t\tBalance: "+"$"+balance);
+            }else if(action==3){
+                System.out.println("Here is your current balance: "+"$"+balance);
+            }else if(action==4){
+                System.out.println("Thanks for visiting! Have a nice day!");
+                running=false;
+            }
+
+            //System.out.print("\nWhat action would you like to perform?");
         }
+        
+        kb.close();
         
 
     }
 
+
 }
- 
+
+  
